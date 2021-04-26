512d26d2696c0e3d4be2cde14faefce4b8d0a5a2
Felix Itiel Arino Gila <FelixItiel.Arino@tajamar365.com>
2021-04-26 12:47:34 +0200

    cambios para probar en nuestra maquina EC2

---
+++ MvcCoreAWSPostgres/Repositories/RepositoryDepartamento.cs
@@ -0,0 +1,29 @@
+﻿using MvcCoreAWSPostgres.Data;
+using MvcCoreAWSPostgres.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreAWSPostgres.Repositories
+{
+    public class RepositoryDepartamento
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamento (DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Departamento> GetDepartamentos()
+        {
+            return this.context.Departamentos.ToList();
+        }
+
+        public Departamento FindDepartamento( int id )
+        {
+            return this.context.Departamentos.SingleOrDefault(x => x.IdDepartamento == id);
+        }
+    }
+}
--- MvcCoreAWSPostgres/Startup.cs
+++ MvcCoreAWSPostgres/Startup.cs
@@ -5,17 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcCoreAWSPostgres.Data;
+using MvcCoreAWSPostgres.Repositories;
 
 namespace MvcCoreAWSPostgres
 {
     public class Startup
     {
+        private IConfiguration Configuration;
+        public Startup (IConfiguration config)
+        {
+            this.Configuration = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string cadena = Configuration.GetConnectionString("postgreslocal");
+            services.AddTransient<RepositoryDepartamento>();
+            services.AddDbContext<DepartamentosContext>(options => options.UseNpgsql(cadena));
             services.AddControllersWithViews();
         }
 
